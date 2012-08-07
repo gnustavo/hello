dc22d6d2cf5b6a1d8e5fae26edaac0d5dd4a9937
Gustavo Chaves <gustavo@cpqd.com.br>
2012-08-07 10:26:40 -0300

    Two new functions.

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/* Another function */
+void another_function()
+{
+    printf("Another function.\n");
+}
+
+/* Yet another function */
+void yet_another_function()
+{
+    printf("Yet another function\n");
+}
+
 /* Main function */
 int main()
 {
     printf("Hello world!\n");
 }
-
